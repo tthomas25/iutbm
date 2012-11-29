0febd25d5080ea9295ff40bbd81521fb5252c5b0
Clément Boulachin <couc.boulacgmail.com>
2012-11-29 16:54:27 +0100

    Ajout des vues

---
+++ iutbm/SeventhTopViewController.h
@@ -0,0 +1,16 @@
+//
+//  SeventhTopViewController.h
+//  iutbm
+//
+//  Created by projet2012 on 29/11/12.
+//  Copyright (c) 2012 IOS_iut. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <QuartzCore/QuartzCore.h>
+#import "ECSlidingViewController.h"
+#import "MenuViewController.h"
+
+@interface SeventhTopViewController : UIViewController
+- (IBAction)revealMenu:(id)sender;
+@end
---
+++ iutbm/SixthTopViewController.h
@@ -0,0 +1,16 @@
+//
+//  SixthTopViewController.h
+//  iutbm
+//
+//  Created by projet2012 on 29/11/12.
+//  Copyright (c) 2012 IOS_iut. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <QuartzCore/QuartzCore.h>
+#import "ECSlidingViewController.h"
+#import "MenuViewController.h"
+
+@interface SixthTopViewController : UIViewController
+- (IBAction)revealMenu:(id)sender;
+@end
